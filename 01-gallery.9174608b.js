a429142dd1f18a74f99851593e8ef23c7d73e2bc
YuliaPysarenko <107835465+YuliaPysarenko@users.noreply.github.com>
2023-04-13 19:49:03 +0000

    Deploying to gh-pages from @ YuliaPysarenko/goit-js08-hw@cdc719a9bfcec179e7a39040b39175596b56e67c 🚀

--- 01-gallery.9174608b.js
+++
@@ -1,2 +0,0 @@
-!function(){console.log(SimpleLightbox),console.log(galleryItemsEl);new SimpleLightbox(".gallery li a",{captionsData:"alt",captionSelector:"img",captionType:'img alt="${description}"',captionPosition:"bottom",captionDelay:250})}();
-//# sourceMappingURL=01-gallery.9174608b.js.map
