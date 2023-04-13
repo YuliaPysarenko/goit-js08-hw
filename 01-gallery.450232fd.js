104257e0b9e0743b3e68ba38516b5644f0510de7
YuliaPysarenko <107835465+YuliaPysarenko@users.noreply.github.com>
2023-04-13 18:45:22 +0000

    Deploying to gh-pages from @ YuliaPysarenko/goit-js08-hw@10f287ae242c775864f7a976ce3d0b97f5d8224a 🚀

---
+++ 01-gallery.450232fd.js
@@ -0,0 +1,2 @@
+console.log(SimpleLightbox),console.log(galleryItemsEl);new SimpleLightbox(".gallery li a",{captionsData:"alt",captionSelector:"img",captionType:'img alt="${description}"',captionPosition:"bottom",captionDelay:250});
+//# sourceMappingURL=01-gallery.450232fd.js.map
