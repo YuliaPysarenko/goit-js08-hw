f76e33e899df6050b276dbd3e0ff7f5b51b38f3c
YuliaPysarenko <y.pisarenko@ukr.net>
2023-04-30 16:26:35 +0300

    home

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,36 +1,32 @@
 import VimeoPlayer from "@vimeo/player";
 import LodashThrottle from "lodash.throttle";
 
-
-
 const iframe = document.querySelector('iframe');
 const player = new VimeoPlayer(iframe);
 
 player.on('timeupdate', LodashThrottle(onPlay, 1000));
- 
-// function prDefault(evt) {
-//     evt.preventDefault();
-// }
-// console.log(prDefault);
+
+ saveVideo = localStorage.getItem(`videoplayer-current-time`);
 
 function onPlay(data) {
     localStorage.setItem(`videoplayer-current-time`, data.seconds);
 };
 
-const saveVideo = localStorage.getItem(`videoplayer-current-time`);
+   if (saveVideo) {
+          player.setCurrentTime(saveVideo).then(function (seconds) {
+               console.log(saveVideo)
+                // seconds = the actual time that the player seeked to
+            }).catch (function (error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
 
-player.setCurrentTime(saveVideo).then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-});
-// .catch (function (error) {
-//     switch (error.name) {
-//         case 'RangeError':
-//             // the time was less than 0 or greater than the video’s duration
-//             break;
-
-//         default:
-//             // some other error occurred
-//             break;
-//     }
-// });
-
+            default:
+                // some other error occurred
+                break;
+        }
+    });
+            
+};
+ 
